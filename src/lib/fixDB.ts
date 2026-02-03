6f2084f8d4aea0d14acb321fed6dd26ea0aa2ff6
ACT丶流星雨 <1340145680@qq.com>
2026-02-03 18:27:09 +0800

    no message

--- src/lib/fixDB.ts
+++ src/lib/fixDB.ts
@@ -1,13 +1,17 @@
 import { Knex } from "knex";
 
 export default async (knex: Knex): Promise<void> => {
-  const hasTime = await knex.schema.hasColumn("t_video", "time");
-  if (!hasTime) {
+  const videoHasTime = await knex.schema.hasColumn("t_video", "time");
+  if (!videoHasTime) {
     await knex.schema.alterTable("t_video", (table) => {
       table.integer("time");
     });
-    console.log("字段 'time' 已添加到 t_video 表");
-  } else {
-    console.log("t_video 表已存在 'time' 字段");
+  }
+
+  const configHasIndex = await knex.schema.hasColumn("t_config", "index");
+  if (configHasIndex) {
+    await knex.schema.alterTable("t_config", (table) => {
+      table.dropColumn("index");
+    });
   }
 };
