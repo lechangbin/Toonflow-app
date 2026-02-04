1e4566eaa6d6e7157bf2ba21ea74750180a3f6a8
zhishi <1951671751@qq.com>
2026-02-04 14:44:34 +0800

    更新api设置，更新所有涉及ai调用方法

--- src/utils.ts
+++ src/utils.ts
@@ -8,12 +8,15 @@ import getConfig from "./utils/getConfig";
 import { v4 as uuid } from "uuid";
 
 import AIText from "@/utils/ai/text";
-
+import generateVideo from "@/utils/ai/generateVideo";
+import generateImage from "@/utils/ai/generateImage";
 export default {
   db,
   oss,
   ai: {
     text: AIText,
+    generateVideo,
+    generateImage,
   },
   editImage,
   number2Chinese,
