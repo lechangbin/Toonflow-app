091707520b026438a3a532d1018f42cbb74b3d29
小帅 <2944435683>
2026-03-23 15:13:38 +0800

    生成视频

---
+++ src/routes/production/workbench/videoPolling.ts
@@ -0,0 +1,19 @@
+import express from "express";
+import u from "@/utils";
+import { z } from "zod";
+import { success } from "@/lib/responseFormat";
+import { validateFields } from "@/middleware/middleware";
+const router = express.Router();
+
+export default router.post(
+  "/",
+  validateFields({
+    scriptId: z.number(),
+    specifyIds: z.array(z.number()),
+  }),
+  async (req, res) => {
+    const { scriptId, specifyIds } = req.body;
+    const data = await u.db("o_video").where("scriptId", scriptId).whereIn("id", specifyIds).select("*");
+    res.status(200).send(success(data));
+  },
+);
