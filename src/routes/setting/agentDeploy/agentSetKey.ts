a9f3ed2349e2fca1d2167ab85745c9411d5896d8
ACT丶流星雨 <1340145680@qq.com>
2026-04-03 00:26:34 +0800

    修复横竖屏问题，修复一键填入问题

--- src/routes/setting/agentDeploy/agentSetKey.ts
+++ src/routes/setting/agentDeploy/agentSetKey.ts
@@ -1,5 +1,5 @@
 import express from "express";
-import { success } from "@/lib/responseFormat";
+import { success, error } from "@/lib/responseFormat";
 import u from "@/utils";
 import { z } from "zod";
 import { validateFields } from "@/middleware/middleware";
@@ -8,15 +8,50 @@ const router = express.Router();
 export default router.post(
   "/",
   validateFields({
-    id: z.array(z.number()),
+    key: z.string(),
   }),
   async (req, res) => {
-    const { id } = req.body;
-    await u.db("o_agentDeploy").whereIn("id", id).where("disabled", "<>", 1).update({
-      model: "gpt-4.1",
-      modelName: "toonflow:gpt-4.1",
-      vendorId: "toonflow",
-    });
-    res.status(200).send(success("配置成功"));
+    const { key } = req.body;
+    const vendorConfigData = await u.db("o_vendorConfig").where("id", "toonflow").first();
+    if (!vendorConfigData) return res.status(500).send(error("未找到该供应商配置"));
+    if (!vendorConfigData.inputValues) return res.status(500).send(error("未找到模型配置数据"));
+    const inputValue = JSON.parse(vendorConfigData.inputValues!);
+    inputValue.apiKey = key;
+    await u
+      .db("o_vendorConfig")
+      .where("id", "toonflow")
+      .update({
+        inputValues: JSON.stringify(inputValue),
+      });
+    try {
+      const resText = await u.Ai.Text(`toonflow:gpt-4.1`).invoke({
+        prompt: "1+1等于几？",
+      });
+      if (resText.text) {
+        await u.db("o_agentDeploy").where("key", "scriptAgent").update({
+          model: "claude-sonnet-4-6",
+          modelName: "toonflow:claude-sonnet-4-6",
+          vendorId: "toonflow",
+        });
+        await u.db("o_agentDeploy").where("key", "productionAgent").update({
+          model: "claude-sonnet-4-6",
+          modelName: "toonflow:claude-sonnet-4-6",
+          vendorId: "toonflow",
+        });
+        await u.db("o_agentDeploy").where("key", "universalAi").update({
+          model: "claude-haiku-4-5",
+          modelName: "toonflow:claude-haiku-4-5-20251001",
+          vendorId: "toonflow",
+        });
+        res.status(200).send(success("一键填入成功"));
+      }
+    } catch (err) {
+      inputValue.apiKey = "";
+      await u
+        .db("o_vendorConfig")
+        .where("id", "toonflow")
+        .update({ inputValues: JSON.stringify(inputValue) });
+      res.status(400).send(error("KEY无效，请重新输入"));
+    }
   },
 );
