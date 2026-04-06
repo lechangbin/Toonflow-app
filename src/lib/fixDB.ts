7f54c3a7faae2e1b94079630b9866996665f343c
zhishi <1951671751@qq.com>
2026-04-06 22:15:13 +0800

    增加 生成中状态 在软件重启时 改为失败

--- src/lib/fixDB.ts
+++ src/lib/fixDB.ts
@@ -1,5 +1,5 @@
 import { Knex } from "knex";
-
+import db from "@/utils/db";
 export default async (knex: Knex): Promise<void> => {
   const addColumn = async (table: string, column: string, type: string) => {
     if (!(await knex.schema.hasTable(table))) return;
@@ -23,4 +23,28 @@ export default async (knex: Knex): Promise<void> => {
       });
     }
   };
+  await db("o_novel").where("eventState", 0).update({
+    eventState: -1,
+    errorReason: "软件退出导致失败",
+  });
+  await db("o_script").where("extractState", 0).update({
+    extractState: -1,
+    errorReason: "软件退出导致失败",
+  });
+  await db("o_assets").where("promptState", "生成中").update({
+    promptState: "生成失败",
+    promptErrorReason: "软件退出导致失败",
+  });
+  await db("o_image").where("state", "生成中").update({
+    state: "生成失败",
+    errorReason: "软件退出导致失败",
+  });
+  await db("o_storyboard").where("state", "生成中").update({
+    state: "生成失败",
+    reason: "软件退出导致失败",
+  });
+  await db("o_video").where("state", "生成中").update({
+    state: "生成失败",
+    errorReason: "软件退出导致失败",
+  });
 };
