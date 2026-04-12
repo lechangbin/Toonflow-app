8541aa31b32bcc15cea7888e9f03b6e7d6e6439d
a12110 <air.s.li2233@gmail.com>
2026-04-12 17:00:33 +0800

    更新: 删除 通过 .env 读入 ossurl 环境变量

--- src/env.ts
+++ src/env.ts
@@ -1,19 +1,9 @@
-import dotenv from "dotenv";
-import path from "node:path";
-
 // 判断是否为打包后的 Electron 环境
 const isElectron = typeof process.versions?.electron !== "undefined";
 let isPackaged = false;
 if (isElectron) {
   const { app } = require("electron");
   isPackaged = app.isPackaged;
-}
-
-// 仅在非 Electron 环境加载 .env（例如 Docker / Web 后端）
-if (!isElectron) {
-  dotenv.config({
-    path: path.resolve(process.cwd(), ".env"),
-  });
 }
 
 //加载环境变量（打包环境默认使用 prod）
--- src/utils/oss.ts
+++ src/utils/oss.ts
@@ -51,7 +51,7 @@ class OSS {
     const safePath = normalizeUserPath(userRelPath);
     // URL 始终使用 /，所以这里需要将系统分隔符转回 /
     let url = `/${prefix}/`;
-    if (!process.env.ossURL || process.env.ossURL != "") url = process.env.ossURL + `/${prefix}/`;
+    if (process.env.ossURL && process.env.ossURL !== "") url = process.env.ossURL + `/${prefix}/`;
     if (process.env.NODE_ENV == "dev") url = `http://localhost:10588/${prefix}/`;
     if (isEletron()) url = `http://localhost:${process.env.PORT}/${prefix}/`;
     return `${url}${safePath.split(path.sep).join("/")}`;
